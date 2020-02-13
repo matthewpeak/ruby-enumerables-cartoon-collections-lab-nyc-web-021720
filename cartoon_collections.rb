5526e6053c5cf2550e3d342660211085ccba5349
Matthew Peak <matthewpeak@gmail.com>
2020-02-13 20:53:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def summon_captain_planet(arr)# code an argument here
   # Your code her
-  return arr.map{|elem| elem}
+  return arr.map{|elem| elem.capitalize+"!"}
 end
 
 def long_planeteer_calls# code an argument here
