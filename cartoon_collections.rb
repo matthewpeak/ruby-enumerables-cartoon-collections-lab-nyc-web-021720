cb1cbcd7037b63f97f25ff192ff1e0970d998385
Matthew Peak <matthewpeak@gmail.com>
2020-02-13 21:05:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,8 @@ def long_planeteer_calls(arr)# code an argument here
 return  arr.filter{|elem| elem.length > 4}.length >=1
 end 
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
